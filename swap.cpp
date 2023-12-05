db81428b3f8374f55bfd582d64b5621df1eb143b
RUPESH0004 <rj9082356865@gmail.com>
2023-12-05 19:14:25 +0530

    working on reversing an array added new function for reversing thir addres

--- swap.cpp
+++ swap.cpp
@@ -1,42 +1,51 @@
-//swaping alternate no 
+// swaping alternate no
 #include <iostream>
 using namespace std;
-//swaping with the use of swap function
-void reverse(int arr[],int size)
-{
-    int first=0;
-    int last = size-1;
-    for(int i=0;i<size;i++)
-    {
-       swap(arr[i],arr[last]);
-        first++;
-        last--;
-    }
-}
+// reverse printing of an arry
 void swap(int arr[], int size)
 {
 
-    for (int i = 0; i < size; i+=2)
+    for (int i = 0; i < size; i += 2)
     {
-        if (i+1<size)
+        if (i + 1 < size)
         {
             swap(arr[i], arr[i + 1]);
         }
+        cout << arr[i] << " " << arr[i + 1] << " ";
     }
+
+    cout << endl;
 }
-//swaping without swap function 
+void reversingPosition(int arr[], int size)
+{
+    for (int i = 0, j = size - 1; i < j; i++, j--)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    for (int i = 0; i <= size - 1; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+// swaping without swap function
 void swapingw(int arr[], int size)
 {
     int first;
-    for (int i = 0; i <= size - 1; i+=2)
+    for (int i = 0; i <= size - 1; i += 2)
     {
-        if (i+1<size)
+        if (i + 1 < size)
         {
-             first=arr[i+1] ;
-            arr[i+1]=arr[i];
+            first = arr[i + 1];
+            arr[i + 1] = arr[i];
             arr[i] = first;
         }
+
+        cout << arr[i] << " " << arr[i + 1] << " ";
     }
+    cout << endl;
 }
 void printing(int arr[], int size)
 {
@@ -51,5 +60,4 @@ int main()
     int arr[6] = {1, 4, 6, 4, 7, 5};
    reverse(arr,6);
    printing(arr,6);
-
 }
